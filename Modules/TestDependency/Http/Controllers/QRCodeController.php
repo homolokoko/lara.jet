152d7c5b3fc7f315bfaa05d241254f80c4791578
Hengtong-sou <souhengtong480@gmail.com>
2025-05-26 15:11:14 +0700

    refactor(qr-code): generate qr code

--- Modules/TestDependency/Http/Controllers/QRCodeController.php
+++ Modules/TestDependency/Http/Controllers/QRCodeController.php
@@ -15,7 +15,7 @@ class QRCodeController extends Controller
      */
     public function index()
     {
-        return response()->json(TblTicketEntity::paginate(50));
+        return response()->json(TblTicketEntity::where('orderno','IA14489')->paginate(50));
     }
 
     /**
